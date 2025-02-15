63b7aedff070c906ca7018ba87112bf845e11462
Tatiana Tikhonova <tanianova@yandex.ru>
2025-02-15 13:32:44 +0300

    136. Single Number

---
+++ single-number.js
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/single-number/description/
+// дан массив с числами, в нем все числа повторяются по 2 раза, кроме одного числа. Его и нужно найти.
+// Есть еще одно ограничение: необходимо решить эту задачу за линейное время.
+const input = [2, 2, 1, 1, 4] // 4
+
+// сложность алгоритма по времени и по памяти линейная O(n)
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const singleNumber = function (nums) {
+    let unique = Array.from(new Set(nums))
+    let reduceSum = (s, i) => s + i
+
+    let uniqueSum = unique.reduce(reduceSum)
+    let numSum = nums.reduce(reduceSum)
+
+    return uniqueSum * 2 - numSum
+};
+
+console.log(singleNumber(input));
