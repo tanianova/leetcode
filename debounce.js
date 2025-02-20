56c62c96b01daae1c81c1d91ed3cb46a4d8767bd
Tatiana Tikhonova <tanianova@yandex.ru>
2025-02-20 20:39:03 +0300

    2627. Debounce

---
+++ debounce.js
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/debounce/description/
+
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+const debounce = function (fn, t) {
+    let timer;
+
+    return function(...args){
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            fn(...args)
+        }, t)
+    }
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
