642578cba5e6cb9e0bd4715ef45e8c1fd58f31da
Tatiana Tikhonova <tanianova@yandex.ru>
2025-03-07 08:47:00 +0300

    169. Majority Element

---
+++ array/169.MajorityElement.js
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/majority-element/description/
+// Преобладающий элемент — элемент, который встречается в массиве более ⌊n / 2⌋ раз.
+// Предполагается, что в массиве всегда есть преобладающий элемент.
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const majorityElement = function (nums) {
+    let map = {}
+    let k = Math.floor(nums.length / 2); // преобладающий элемент
+    for (let i = 0; i < nums.length; i++) {
+        let curr = nums[i]
+        if (map[curr]) {
+            map[curr] += 1
+        } else {
+            map[curr] = 1
+        }
+        if (map[curr] > k) {
+            return curr
+        }
+    }
+
+};
+
+let nums = [3, 2, 3]
+console.log(majorityElement(nums)); // 3
+nums = [2, 2, 1, 1, 1, 2, 2]
+console.log(majorityElement(nums)); // 2
