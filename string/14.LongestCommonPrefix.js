a2ce875104b84a43f0a0f2e271f52c918143f9a7
Tatiana Tikhonova <tanianova@yandex.ru>
2025-03-09 09:00:43 +0300

    14. Longest Common Prefix

--- string/14.LongestCommonPrefix.js
+++ string/14.LongestCommonPrefix.js
@@ -1,4 +1,4 @@
-// https://leetcode.com/problems/longest-common-prefix/description/
+// https://leetcode.com/problems/longest-common-prefix/description/?envType=study-plan-v2&envId=top-interview-150
 // Дан массив строк strs, найдите и верните самую длинную общую префиксную строку среди этого массива строк.
 // Если общего префикса нет, верните пустую строку "".
 
@@ -7,6 +7,18 @@
  * @return {string}
  */
 const longestCommonPrefix = function (strs) {
+    if (strs.length === 0) return ""
+    let prefix = strs[0]
+    for (let i = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(prefix) === -1) {
+            prefix = prefix.slice(0, -1)
+            if (prefix === '') return ''
+        }
+    }
+    return prefix
+};
+
+const longestCommonPrefix2 = function (strs) {
     let prev = strs[0]
     let prevLen = prev.length;
     for (let i = 1; i < strs.length; i++) {
@@ -17,11 +29,11 @@ const longestCommonPrefix = function (strs) {
             prev= prev.substring(0,prevLen);
         }
     }
-   return prev
-
+    return prev
 };
 
-const strs = ["flower", "flow", "flight"]
-const strs1 = ["dog", "racecar", "car"]
-console.log(longestCommonPrefix(strs)); // fl
-console.log(longestCommonPrefix(strs1)); // ''
+
+let strs = ["flower", "flow", "flight"]
+console.log(longestCommonPrefix(strs)) // "fl"
+// strs = ["dog", "racecar", "car"]
+// console.log(longestCommonPrefix(strs)) // ""
