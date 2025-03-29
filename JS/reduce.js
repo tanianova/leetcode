4153ad2178511f7e3ed9ea338cc78d17c4be86d9
Tatiana Tikhonova <tanianova@yandex.ru>
2025-03-29 20:45:39 +0300

    reduce

--- JS/reduce.js
+++ JS/reduce.js
@@ -7,7 +7,9 @@
  * @return {number}
  */
 const reduce = function (nums, fn, init) {
-    let result = init
+    if (nums.length === 0) return init;
+
+    let result = init ?? 0;
     for (let i = 0; i < nums.length; i++) {
         result = fn(result, nums[i])
     }
